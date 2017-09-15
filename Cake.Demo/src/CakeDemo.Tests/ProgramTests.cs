8a7bd9c015b8423f72a776dc21759d22a15a21f7
Srivastava <devabs@ttx.com>
2017-09-15 06:04:14 -0500

    code submitted

---
+++ Cake.Demo/src/CakeDemo.Tests/ProgramTests.cs
@@ -0,0 +1,34 @@
+﻿using Ploeh.AutoFixture.Xunit2;
+using Shouldly;
+using System.Collections.Generic;
+using Xunit;
+
+namespace CakeDemo.Tests
+{
+    public class ProgramTests
+    {
+        [Theory, AutoData]
+        public void Should_return_Hello_Name(string name)
+        {
+            var result = Program.SayHello(name);
+            result.ShouldBe($"Hello {name}");
+        }
+
+        [Theory, AutoData]
+        public void Should_return_Bye_Name(string name)
+        {
+            var result = Program.SayGoodBye(name);
+            result.ShouldBe($"GoodBye {name}");
+        }
+        [Theory, AutoData]
+        public void Should_return_List(List<string> names)
+        {
+            names.Add("X");
+            names.Add("Y");
+            names.Add("Z");
+
+            var result = Program.GetNames();
+            result.ShouldBe(names);
+        }
+    }
+}
---
+++ Cake.Demo/src/CakeDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CakeDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = args.Any() ? args[0] : "World";
+            Console.WriteLine(SayHello(name));
+            foreach(var i in GetNames())
+            {
+                Console.WriteLine(i);
+            }
+           
+            Console.ReadKey();
+
+        }
+
+        public static string SayHello(string name)
+        {
+            return $"Hello {name}";
+        }
+        public static string SayGoodBye(string name)
+        {
+            return $"GoodBye {name}";
+        }
+
+        public static List<string> GetNames()
+        {
+            List<string> names = new List<string>();
+            names.Add("X");
+            names.Add("Y");
+            names.Add("Z");
+           
+            return names;
+        }
+    }
+}
